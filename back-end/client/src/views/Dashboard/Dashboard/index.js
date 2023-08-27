1119548cfc9fdb6d03c109bb924ed6618fe80d69
minsunsh <minsunsh@usc.edu>
2023-08-27 12:28:47 -0700

    graph modified

--- back-end/client/src/views/Dashboard/Dashboard/index.js
+++ back-end/client/src/views/Dashboard/Dashboard/index.js
@@ -14,21 +14,34 @@ import LineChart from "components/Charts/LineChart";
 import GraphComponent from 'components/Custom/GraphComponent'; 
 import EventForm from 'components/Custom/EventForm';     
 
+import Img1 from "assets/img/pics/img1.png";
+import Img2 from "assets/img/pics/img2.png";
+import Img3 from "assets/img/pics/img3.png";
+import Img4 from "assets/img/pics/img4.png";
+import Img5 from "assets/img/pics/img5.png";
+import Img6 from "assets/img/pics/img6.png";
+import Img7 from "assets/img/pics/img7.png";
+import Img8 from "assets/img/pics/img8.png";
+import Img9 from "assets/img/pics/img9.png";
+import Img10 from "assets/img/pics/img10.png";
+import Img11 from "assets/img/pics/img11.png";
+import Img12 from "assets/img/pics/img12.png";
+
 
 // ### Test json Data ###
 const jsonData = [
-  { x: "1", y: 13, pic: "test_usc.png", name: "name" },
-  { x: "2", y: 44, pic: "image_url_2", name: "name" },
-  { x: "3", y: 27, pic: "image_url_3", name: "name" },
-  { x: "4", y: 71, pic: "image_url_4", name: "name" },
-  { x: "5", y: -87, pic: "image_url_5", name: "name" },
-  { x: "6", y: -50, pic: "image_url_6", name: "name" },
-  { x: "7", y: 27, pic: "image_url_7", name: "name" },
-  { x: "8", y: 68, pic: "image_url_8", name: "name" },
-  { x: "9", y: -13, pic: "image_url_9", name: "name" },
-  { x: "10", y: 54, pic: "image_url_10", name: "name" },
-  { x: "11", y: 57, pic: "image_url_11", name: "name" },
-  { x: "12", y: 42, pic: "image_url_12", name: "name" }
+  { x: "1", y: 13, pic: Img1, name: "soccer" },
+  { x: "2", y: 44, pic: Img2, name: "trip2" },
+  { x: "3", y: 27, pic: Img3, name: "trip3" },
+  { x: "4", y: 93, pic: Img4, name: "trip4" },
+  { x: "5", y: -87, pic: Img5, name: "trip5" },
+  { x: "6", y: -50, pic: Img6, name: "trip6" },
+  { x: "7", y: 27, pic: Img7, name: "trip7" },
+  { x: "8", y: 68, pic: Img8, name: "trip8" },
+  { x: "9", y: -13, pic: Img9, name: "trip9" },
+  { x: "10", y: 54, pic: Img10, name: "trip10" },
+  { x: "11", y: 57, pic: Img11, name: "marriage" },
+  { x: "12", y: 42, pic: Img12, name: "retirement" },
 ];
 // ########################
 
